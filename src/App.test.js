902e8b01b9771b5363b71718553cc80f95b6cf4a
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-29 18:11:39 -0700

    mocked axios library

--- src/App.test.js
+++ src/App.test.js
@@ -5,17 +5,29 @@ import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 
+jest.mock("axios", () => {
+   return {
+      get: jest.fn((url) => {
+         return Promise.resolve({
+            data: {
+               message: ["foo.jpg", "bar.jpg"]
+            }
+         });
+      })
+   };
+});
+
 // this just allows react-testing-library to do some
 // routine cleanup after each test runs (to reset the DOM and such)
 afterEach(rtl.cleanup);
 
-test.only("Render the heading", async () => {
+test("Render the heading", async () => {
    // render our React app into an in-memory DOM so we can test against it
    const wrapper = rtl.render(<App />);
    await wrapper.findAllByAltText(/dog/i);
 
    wrapper.debug();
-   
+
    // element is now our dom element that we can test against
    const element = wrapper.getByText(/the dog website/i);
 
@@ -23,8 +35,10 @@ test.only("Render the heading", async () => {
    expect(element).toBeVisible();
 });
 
-test("Render count input", () => {
+test("Render count input", async () => {
    const wrapper = rtl.render(<App />);
+   await wrapper.findAllByAltText(/dog/i);
+
    // using a regular expression instead of a string allows our
    // query to be much more flexible. for example, if the text becomes
    // all uppercase, we don't want our test to break
@@ -32,8 +46,9 @@ test("Render count input", () => {
    expect(element).toHaveValue(1);
 });
 
-test("<App .> snapshot", () => {
+test("<App .> snapshot", async () => {
    const wrapper = rtl.render(<App />);
+   await wrapper.findAllByAltText(/dog/i);
 
    expect(wrapper.asFragment()).toMatchSnapshot();
 });
